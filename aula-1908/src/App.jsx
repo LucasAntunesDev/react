34fd4c9f39d3ae8c1310f945d5bfd8a148117ffa
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-08-26 15:25:36 -0300

    add: Aula 26/08 - Manipulando eventos com formulários

--- aula-1908/src/App.jsx
+++ aula-1908/src/App.jsx
@@ -1,32 +1,75 @@
 import {useState} from 'react'
 
-const Header = ({state}) => {
+import './App.css'
+
+//Função para não precisar chamar preventDefault()
+//em vários formulários na aplicação.
+//Dá pra adaptar pra usar outra função também, como validação de formulários.
+//Em linhas gerais, já resolve algumas regras nela, evitando repetição de código
+const onSubmit = callBack => {
+  return event => {
+    event.preventDefault()
+    callBack()
+  }
+}
+
+//todos os atributos de uma tag react são, na verdade, parte de um objeto javascript.
+//é por isso que usamos props.propiedade para acessá-la ou, então,
+//desestrututamos ela
+const Header = ({add}) => {
+  const [nome, setNome] = useState('')
+
+  const handleSubmit = () => {
+    if (nome === '') return
+    //o que usuário digitar será colocado no array
+    add(nome)
+    //limpa o valor do input
+    setNome('')
+  }
+
   return (
     <>
       <header>
-        <span>{state}</span>
-        <span>span 2</span>
+        <form onSubmit={onSubmit(handleSubmit)}>
+          <input
+            type="text"
+            name="nome"
+            value={nome}
+            onChange={({target}) => setNome(target.value)}
+          />
+          <button disabled={!nome}>Adicionar</button>
+        </form>
       </header>
     </>
   )
 }
 
-const Content = ({state, setState}) => {
+const Content = ({state}) => {
   return (
     <main>
-      <button type="button" onClick={() => setState(state+1)}>Clique aqui</button>
+      <ul>
+        {state.map((object, index) => (
+          <li key={index}>{object}</li>
+        ))}
+      </ul>
     </main>
   )
 }
 
 const App = () => {
-  const [state, setState] = useState(13)
+  const [state, setState] = useState([])
+  //recebe um valor e altera um estado do que está dentro do contructor
+  //três pontos => cria uma cópia de state,
+  //valor a a "cópia" são inseridos em state
+  const add = valor => setState([...state, valor])
 
   return (
     <>
-      <Header state={state} />
+      {/* header é responsável por adicionar informações */}
+      <Header add={add} />
       <aside></aside>
-      <Content state={state} setState={setState}/>
+      {/* header é responsável por mostrar informações */}
+      <Content state={state} />
       <footer></footer>
     </>
   )
